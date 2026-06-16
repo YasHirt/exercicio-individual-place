6f814f8e292afb9216e7c4d2c38e7624979b8fe1
Yasmin Hirt <yasmin.hirt@placeti.com>
2026-06-16 14:56:37 -0300

    Commit inicial e tabela genero

---
+++ src/main/java/com/placeti/projetoExercicioIndividual/ProjetoExercicioIndividualApplication.java
@@ -0,0 +1,13 @@
+package com.placeti.projetoExercicioIndividual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoExercicioIndividualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoExercicioIndividualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/placeti/projetoExercicioIndividual/ProjetoExercicioIndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placeti.projetoExercicioIndividual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoExercicioIndividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
